8bd2eddcbbdade812ff8a1e5d0b8d471f5e4e2ae
Rodrigo Barbieri <rodrigo.barbieri@fit-tecnologia.org.br>
2016-07-19 11:15:17 -0300

    Add Share Migration support
    Add share migration support to manila-ui.
    
    Share migration row buttons are present in Shares table from
    Admin section.
    
    Change-Id: I2af0f56c17b8ca949b068a60314349686b81b34d
    Implements: blueprint share-migration

--- manila_ui/enabled/_90_manila_admin_shares.py
+++ manila_ui/enabled/_90_manila_admin_shares.py
@@ -2,3 +2,9 @@ PANEL_DASHBOARD = 'admin'
 PANEL_GROUP = 'admin'
 PANEL = 'shares'
 ADD_PANEL = 'manila_ui.dashboards.admin.shares.panel.Shares'
+
+# The OPENSTACK_MANILA_FEATURES settings can be used to enable or disable
+# various services provided by manila.
+OPENSTACK_MANILA_FEATURES = {
+    'enable_migration': True,
+}
